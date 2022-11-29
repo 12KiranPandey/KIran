adcb4aff59ebc66133818b00dc7d35a1bbb30bcf
12kiranpandey <kiranpandey14072001@gmail.com>
2022-11-29 13:54:42 +0530

    hjdh

---
+++ 1st webpage/index.js
@@ -0,0 +1,18 @@
+// let a=prompt("Enter first Number");
+// let b=prompt("Enter sec number");
+// let temp;
+// temp=a;
+// a=b;
+// b=temp;
+// console.log(`a after swap`${a});
+// console.log('b after swap ${b}');
+var i;
+var a=[10,11,12,13,14,15,16,17,18,19,20];
+// var b=a.filter((e)=>{e%2==0})
+// console.log(b);
+for(i=0;i<a.length;i++){
+  if(i%2==0){
+
+  }
+}
+
